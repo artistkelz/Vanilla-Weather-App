31a99faf73055b65930c7939a607f3b2807c1534
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-01-25 22:57:08 -0800

    Added the search engine

--- src/index.js
+++ src/index.js
@@ -42,8 +42,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "ea8c7c77cae66eaec84b63ab2240c3da";
-let city = "Los Angeles";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "ea8c7c77cae66eaec84b63ab2240c3da";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+search("Los Angeles");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
