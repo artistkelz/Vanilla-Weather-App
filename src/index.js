dd7d8e79f1048a920b1795126892f6b58a9e2d08
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-01-30 20:17:22 -0800

    Weather forecast, JS Template

--- src/index.js
+++ src/index.js
@@ -21,6 +21,35 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+   <div class="col-2">
+    <div class="weather-forecast-date">${day}</div>
+      <img
+      src="https://openweathermap.org/img/wn/01d@2x.png"
+      alt=""
+      width="42"
+      />
+      <div class="weather-forecast-temperatures">
+      <span class="weather-forecast-temperature-max">18°</span>
+      <span class="weather-forecast-temperature-min">12°</span>
+      </div>
+    </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -85,3 +114,4 @@ let celciusLink = document.querySelector("#celcius-link");
 celciusLink.addEventListener("click", displayCelciusTemp);
 
 search("Los Angeles");
+displayForecast();
