47cee96b410b2f1657bc9b3e3785b1a41a3f23c5
artistkelz <93364676+artistkelz@users.noreply.github.com>
2022-02-01 22:14:41 -0800

    changed icons, fixed up styling

--- src/index.js
+++ src/index.js
@@ -42,11 +42,11 @@ function displayForecast(response) {
    <div class="col-2">
     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
       <img
-      src="https://openweathermap.org/img/wn/${
-        forecastDay.weather[0].icon
-      }@2x.png"
+      class="weather-forecast-icons"
+      src="img/icons/${forecastDay.weather[0].icon}.png"
+      id="icon"
       alt="" 
-      width="42"
+      width="50">
       />
       <div class="weather-forecast-temperatures">
       <span class="weather-forecast-temperature-max">${Math.round(
@@ -66,7 +66,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "ea8c7c77cae66eaec84b63ab2240c3da";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiURL).then(displayForecast);
@@ -90,7 +89,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `img/icons/${response.data.weather[0].icon}.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
